236633525b4a8893cf56ec3ff417113c3ec2f0d0
Maxim DOVHOPOLYI <dovgopolyy0000@gmail.com>
2022-10-15 23:00:40 +0300

    fix(datastore): do not send parent lt if it is null

--- node-red-contrib-rollun-query-from-table/http-datastore.js
+++ node-red-contrib-rollun-query-from-table/http-datastore.js
@@ -1,5 +1,9 @@
 const url = require('url');
-const { resolvePath, getLifecycleToken, defaultLogger } = require('node-red-contrib-rollun-backend-utils');
+const {
+  resolvePath,
+  getLifecycleToken,
+  defaultLogger,
+} = require('node-red-contrib-rollun-backend-utils');
 
 class HttpDatastoreError extends Error {
   constructor(message, request) {
@@ -10,13 +14,18 @@ class HttpDatastoreError extends Error {
 }
 
 class HttpDatastore {
-
   /**
    *
    * @param opts {{URL: string, timeout?: number}}
    * @param timeout {number?}
    */
-  constructor({ URL, idField = 'id', timeout = 10000, msg = {}, logRequest = false }) {
+  constructor({
+    URL,
+    idField = 'id',
+    timeout = 10000,
+    msg = {},
+    logRequest = false,
+  }) {
     if (!URL) throw new Error('Url is required.');
     const { protocol, host, pathname } = url.parse(URL);
     if (!host) throw new Error(`url is not in valid format! [${URL}]`);
@@ -51,8 +60,10 @@ class HttpDatastore {
       headers: {
         'content-type': 'application/json',
         lifecycle_token: lToken,
-        parent_lifecycle_token: plToken,
-      }
+        ...(plToken && {
+          parent_lifecycle_token: plToken,
+        }),
+      },
     });
 
     if (logRequest) {
@@ -96,14 +107,17 @@ class HttpDatastore {
    */
 
   static resolveRQLWithREDMsg(rql, msg = {}) {
-    return rql.trim()
-      // remove trailing ?
-      .replace(/^\?/, '')
-      // resolve path
-      .replace(/msg\.[a-zA-Z0-9.]+/g, match => {
-        const path = match.replace(/^msg\./, '');
-        return HttpDatastore.encodeRQLValue(resolvePath(msg, path));
-      })
+    return (
+      rql
+        .trim()
+        // remove trailing ?
+        .replace(/^\?/, '')
+        // resolve path
+        .replace(/msg\.[a-zA-Z0-9.]+/g, (match) => {
+          const path = match.replace(/^msg\./, '');
+          return HttpDatastore.encodeRQLValue(resolvePath(msg, path));
+        })
+    );
   }
 
   /**
@@ -122,7 +136,7 @@ class HttpDatastore {
         .replace('.', '%2E')
         .replace('~', '%7E')
         .replace('*', '%2A')
-        .replace('\'', '%27')
+        .replace("'", '%27')
         .replace('!', '%21');
     }
     return encodedValue;
@@ -137,21 +151,26 @@ class HttpDatastore {
    */
 
   static _withResponseFormatter(promise, fullResponse = false) {
-    return promise
-      .then(res => {
-        if (res.error) throw new HttpDatastoreError(res.error, res.config);
-        return fullResponse ? res : res.data
-      })
-      // rethrow error, with different message, if key error exists in response
-      .catch(err => {
-        if (err.response && err.response.data) {
-          if (err.response.data.error) {
-            throw new HttpDatastoreError(err.response.data.error, err.config);
+    return (
+      promise
+        .then((res) => {
+          if (res.error) throw new HttpDatastoreError(res.error, res.config);
+          return fullResponse ? res : res.data;
+        })
+        // rethrow error, with different message, if key error exists in response
+        .catch((err) => {
+          if (err.response && err.response.data) {
+            if (err.response.data.error) {
+              throw new HttpDatastoreError(err.response.data.error, err.config);
+            }
+            throw new HttpDatastoreError(
+              JSON.stringify(err.response.data),
+              err.config
+            );
           }
-          throw new HttpDatastoreError(JSON.stringify(err.response.data), err.config);
-        }
-        throw new HttpDatastoreError(err.message, err.config);
-      });
+          throw new HttpDatastoreError(err.message, err.config);
+        })
+    );
   }
 
   /**
@@ -163,9 +182,7 @@ class HttpDatastore {
 
   static _getUri(uri) {
     if (!uri) return '';
-    return `/${uri
-      .replace(/^\//, '')
-      .replace(/\/$/, '')}`;
+    return `/${uri.replace(/^\//, '').replace(/\/$/, '')}`;
   }
 
   /**
@@ -177,8 +194,10 @@ class HttpDatastore {
    */
 
   async query(uri, rql = '', fullResponse = false) {
-    return HttpDatastore._withResponseFormatter(this.axios
-        .get(`${this.pathname}${HttpDatastore._getUri(uri)}?${this.parseRql(rql)}`),
+    return HttpDatastore._withResponseFormatter(
+      this.axios.get(
+        `${this.pathname}${HttpDatastore._getUri(uri)}?${this.parseRql(rql)}`
+      ),
       fullResponse
     );
   }
@@ -192,9 +211,12 @@ class HttpDatastore {
    */
 
   async getFirst(uri, rql = '', fullResponse = false) {
-    return HttpDatastore._withResponseFormatter(this.axios
-        .get(`${this.pathname}${HttpDatastore._getUri(uri)}?${this.parseRql(rql)}`)
-        .then(result => {
+    return HttpDatastore._withResponseFormatter(
+      this.axios
+        .get(
+          `${this.pathname}${HttpDatastore._getUri(uri)}?${this.parseRql(rql)}`
+        )
+        .then((result) => {
           if (result.data && result.data.length > 0) {
             result.data = result.data[0];
           } else {
@@ -207,12 +229,13 @@ class HttpDatastore {
   }
 
   async read(uri, id, fullResponse = false) {
-    return HttpDatastore._withResponseFormatter(this.axios
-        .get(`${this.pathname}${HttpDatastore._getUri(uri)}/${encodeURI(id)}`),
+    return HttpDatastore._withResponseFormatter(
+      this.axios.get(
+        `${this.pathname}${HttpDatastore._getUri(uri)}/${encodeURI(id)}`
+      ),
       fullResponse
     );
   }
-
 
   /**
    *
@@ -223,8 +246,8 @@ class HttpDatastore {
    */
 
   async create(uri, body, fullResponse = false) {
-    return HttpDatastore._withResponseFormatter(this.axios
-        .post(`${this.pathname}${HttpDatastore._getUri(uri)}`, body),
+    return HttpDatastore._withResponseFormatter(
+      this.axios.post(`${this.pathname}${HttpDatastore._getUri(uri)}`, body),
       fullResponse
     );
   }
@@ -240,10 +263,12 @@ class HttpDatastore {
   async update(uri, body, fullResponse = false) {
     const id = body[this.idField];
     if (!id) {
-      throw new Error(`Id field with name [${this.idField}] is empty or does not exist in body!`);
+      throw new Error(
+        `Id field with name [${this.idField}] is empty or does not exist in body!`
+      );
     }
-    return HttpDatastore._withResponseFormatter(this.axios
-        .put(`${this.pathname}${HttpDatastore._getUri(uri)}`, body),
+    return HttpDatastore._withResponseFormatter(
+      this.axios.put(`${this.pathname}${HttpDatastore._getUri(uri)}`, body),
       fullResponse
     );
   }
@@ -257,8 +282,8 @@ class HttpDatastore {
    */
 
   async delete(uri, id, fullResponse = false) {
-    return HttpDatastore._withResponseFormatter(this.axios
-        .delete(`${this.pathname}${HttpDatastore._getUri(uri)}/${id}`),
+    return HttpDatastore._withResponseFormatter(
+      this.axios.delete(`${this.pathname}${HttpDatastore._getUri(uri)}/${id}`),
       fullResponse
     );
   }
@@ -283,7 +308,8 @@ function fixRqlEncoding(rql) {
        *  YYYY-MM-DD hh:mm:ss - non ISO8601 format
        */
 
-      const almostISORegex = /\d{4}(-\d\d(-\d\d([T ]\d\d:\d\d(:\d\d)?(\.\d+)?(([+-]\d\d:\d\d)|Z)?)?)?)?/gi;
+      const almostISORegex =
+        /\d{4}(-\d\d(-\d\d([T ]\d\d:\d\d(:\d\d)?(\.\d+)?(([+-]\d\d:\d\d)|Z)?)?)?)?/gi;
 
       return node
         .replace(almostISORegex, (match) => encodeURIComponent(match))
